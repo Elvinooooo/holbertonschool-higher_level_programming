78734434963bc981bda638751c80406c68638ae9
Elvinooooo <Elvinooooo@github.com>
2024-01-28 04:29:50 -0800

    all done

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    multies = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            multies.append(True)
+        else:
+            multies.append(False)
+
+    return multies
