73d93c2289d951fa36924daafa1b39973a5615ea
Elvinooooo <Elvinooooo@github.com>
2024-02-01 11:12:06 -0800

    all tasks

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,1 +1,3 @@
 #!/usr/bin/python3
+    new_list = list(map(lambda x: x * number, my_list))
+    return new_list
--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string == None:
+        return 0
+    result = 0
+    roman_numerals = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+    }
+    for i in range(0, len(roman_string)):
+        for key in roman_numerals.keys():
+            if key == roman_string[i]:
+                result += roman_numerals[key]
+    return result
