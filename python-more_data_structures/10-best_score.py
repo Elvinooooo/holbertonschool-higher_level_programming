49b33f34794b3e7069d392beb9163d7340328411
Elvinooooo <Elvinooooo@github.com>
2024-02-01 11:23:32 -0800

    tasks

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+    best_key, best_value = max(a_dictionary.items(), key=lambda x: x[1])
+    return best_key
