7f85a1c47846767e6af13dcadc54120880f81f3f
Elvinooooo <Elvinooooo@github.com>
2024-02-03 10:10:17 -0800

    2task

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     quantity = 0
-    try:
-        for i in range(x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                quantity += 1
-            else:
-                continue
-    except IndexError:
-        pass
+    for i in range (x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            quantity += 1
+        except (TypeError, ValueError):
+           continue 
     print()
     return quantity
