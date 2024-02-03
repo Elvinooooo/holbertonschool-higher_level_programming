8b45b64541615d6e32a79745b24907c67c485796
Elvinooooo <Elvinooooo@github.com>
2024-02-03 10:11:50 -0800

    pycodestyle of task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     quantity = 0
-    for i in range (x):
+    for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             quantity += 1
         except (TypeError, ValueError):
-           continue 
+            continue
     print()
     return quantity
