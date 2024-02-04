eaf62a80e42ba702f8d5296f90375877939faad7
Elvinooooo <Elvinooooo@github.com>
2024-02-03 23:03:10 -0800

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""Define a Square class """
+
+
 class Square:
+    """This class represents a Square.
+
+    This class can be used to create square objects.
+    """
     def __init__(self, size=0):
+        """Initialize a Square object with a given size
+        Args:
+            size(int): the size of the square
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
