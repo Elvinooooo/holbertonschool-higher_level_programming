91cbb9f10af68aa9f11c412ac5b9c240eef68727
Elvinooooo <Elvinooooo@github.com>
2024-02-03 23:12:27 -0800

    3 square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
+"""Define a Square class """
+
+
 class Square:
-    def __init__(self, size = 0):
-        if not isistance(size, int):
+    """This class represents a Square.
+
+    This class can be used to create square objects.
+    """
+    def __init__(self, size=0):
+        """Initialize a Square object with a given size
+        Args:
+            size(int): the size of the square
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.__size * self.__size
