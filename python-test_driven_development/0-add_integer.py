3f9f1479e52bb48c09c8e5ccc057c6b661cead1d
Elvinooooo <Elvinooooo@github.com>
2024-02-04 06:37:17 -0800

    added.txt file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""Define an integer addition function."""
 def add_integer(a, b=98):
+    """Return the addition of a and b.
+
+    Float arguments are converted to ints before addition is performed.
+
+    Raises:
+        TypeError: If either  a or b is a non-integer and non-float.
+    """
     if not (isinstance(a, int) or isinstance(a, float)):
         raise TypeError("a must be an integer")
     elif not (isinstance(b, int) or isinstance(b, float)):
