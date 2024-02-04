fe0eae4549ab5c962cf4029c38bf4fcaddc90d15
Elvinooooo <Elvinooooo@github.com>
2024-02-03 22:55:46 -0800

    task 1 documented

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""Define a Square class """
+
+
 class Square:
+    """Define a Square class.
+
+    This class represents a square and can be used to create square objects.
+    """
     def __init__(self, size):
+        """Initialize a Square object with a given size
+        Args:
+            size(int): the size of the square
+        """
         self.__size = size
