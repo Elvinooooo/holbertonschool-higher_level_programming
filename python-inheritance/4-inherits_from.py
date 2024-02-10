654633c169ce6c3d490d75334bd515a2a3b2e1e8
Elvinooooo <Elvinooooo@github.com>
2024-02-10 10:50:46 -0800

    pep8 codestyle

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -11,6 +11,4 @@ def inherits_from(obj, a_class):
 
     Return: True or False
     """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
-    return False
+    return isinstance(obj, a_class) and type(obj) != a_class
