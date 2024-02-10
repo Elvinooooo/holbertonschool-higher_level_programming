0cd171779bfd0882000354fe331a4a29d84c312e
Elvinooooo <Elvinooooo@github.com>
2024-02-10 13:03:06 -0800

    square task

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a Square class inheriting from Rectangle"""
+Rectangle = __import__ ('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represent a Square class"""
+
+    def __init__(self, size):
+        """Initialize the Rectangle
+            Args:
+                size: positive integer
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
