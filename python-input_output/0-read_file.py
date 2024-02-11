03643b9d765f1a66201fe63efe759ff9bb090bb4
Elvinooooo <Elvinooooo@github.com>
2024-02-11 03:09:37 -0800

    adj

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Function that opens file and prints its content"""
 
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
