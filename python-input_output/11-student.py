8c7401eff21314fe3c21c3aba9e61b14cc5f5c90
Elvinooooo <Elvinooooo@github.com>
2024-02-11 07:36:38 -0800

    last

---
+++ python-input_output/11-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Defines a Student class"""
+
+
+class Student:
+    """Represents Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize the class
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The surname of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Convert to json dict"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {
+                attr: getattr(self, attr)
+                for attr in attrs
+                if hasattr(self, attr)
+            }
+
+    def reload_from_json(self, json):
+        """Replaces all attributes"""
+        for key, value in json.items():
+            setattr(self, key, value)
