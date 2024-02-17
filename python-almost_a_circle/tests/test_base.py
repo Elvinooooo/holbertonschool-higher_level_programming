a3da493a363188364dbf86f986d6e26820f9b38c
Elvinooooo <Elvinooooo@github.com>
2024-02-17 13:29:23 -0800

    test case added

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 import unittest
+import os
 from models.base import Base
 
 class TestBase(unittest.TestCase):
     def test_automatic_id(self):
-        base = Base()
-        base = Base()
-        base = Base()
-        self.assertEqual(base.id, 1)
-        self.assertEqual(base.id, 2)
-        self.assertEqual(base.id, 3)
+        base1 = Base()
+        base2 = Base()
+        base3 = Base()
+        self.assertEqual(base1.id, 1)
+        self.assertEqual(base2.id, 2)
+        self.assertEqual(base3.id, 3)
 
 if __name__ == '__main__':
     unittest.main()
