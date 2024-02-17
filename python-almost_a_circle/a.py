74dce07d1ac90b0546e341782aee2fb64e533076
Elvinooooo <Elvinooooo@github.com>
2024-02-17 15:13:02 -0800

    rectangles some cases added

--- python-almost_a_circle/a.py
+++ python-almost_a_circle/a.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-from models.base import Base
-from_json_str = Base.from_json_string('[{ "id": 89 }]')
-print("Result ", from_json_str)
+from models.rectangle import Rectangle
+rec = Rectangle(1, 2, 3, 4, 5)
+print('Rectangle(1, "2") width result is: ', rec.width)
+print('Rectangle(1, "2") height result is: ', rec.height)
+print('Rectangle(1, 2, "3")  y result is:', rec.y)
+print('Rectangle(1, 2, "3") x result is:', rec.x)
+print("result is:", rec)
---
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Module that checks with unit testing the rectangle.py"""
+import unittest
+import os
+from models.rectangle import Rectangle
+import pep8
+
+class TestRectangle(unittest.TestCase):
+    """Class that checks rectangle.py"""
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/rectangle.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def rectangle_1_2(self):
+        """Test of Rectangle(1, 2)"""
+        rectangle = Rectangle(1,2)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+
+    def test_rectangle_1_2_3_exists(self):
+        """Test if Rectangle(1, 2, 3) exists"""
+        rectangle = Rectangle(1, 2, 3)
+        self.assertEqual(rectangle.width, 1)
+        self.assertEqual(rectangle.height, 2)
+        self.assertEqual(rectangle.x, 3)
+
+    def test_rectangle_with_four_param(self):
+        """Rectangle(1, 2, 3, 4)"""
+        rectangle = Rectangle(1, 2, 3, 4)
+        self.assertEqual(rectangle.width, 1)
+        self.assertEqual(rectangle.height, 2)
+        self.assertEqual(rectangle.x, 3)
+        self.assertEqual(rectangle.y, 4)
+
+    def test_rectangle_width_string(self):
+        """Rectangle("1", 2)"""
+        with self.assertRaises(TypeError):
+            Rectangle("1",2)
+
+    def test_rectangle_x_str(self):
+        """Test of Rectangle(1, 2, "3")"""
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, "3")
+
+    def test_rectangle_y_str(self):
+        """Test of Rectangle(1, 2, 3, "4")"""
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3, "4")
+
+    def test_with_5_params(self):
+        """Test of Rectangle(1,2,3,4,5)"""
+        rectangle = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(rectangle.width,1)
+        self.assertEqual(rectangle.height,2)
+        self.assertEqual(rectangle.x,3)
+        self.assertEqual(rectangle.y,4)
+        self.assertEqual(rectangle.id,5)
