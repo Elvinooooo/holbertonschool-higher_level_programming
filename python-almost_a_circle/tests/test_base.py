0be174084247c0623fd0967b99f72a790f485df3
Elvinooooo <Elvinooooo@github.com>
2024-02-17 13:17:00 -0800

    first test case

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+import unittest
+from models.base import Base
 
+class TestBase(unittest.TestCase):
+    def test_automatic_id(self):
+        base = Base()
+
+        self.assertEqual(base.id, 1)
+
+if __name__ == '__main__':
+    unittest.main()
