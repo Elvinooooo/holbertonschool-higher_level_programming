2b0a525aac9e6cda2ae7b1483f9f06037197039a
Elvinooooo <Elvinooooo@github.com>
2024-02-17 13:19:07 -0800

    base auto id itest

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -5,8 +5,10 @@ from models.base import Base
 class TestBase(unittest.TestCase):
     def test_automatic_id(self):
         base = Base()
-
+        base = Base()
         self.assertEqual(base.id, 1)
+        self.assertEqual(base.id, 2)
+        self.assertEqual(base.id, 3)
 
 if __name__ == '__main__':
     unittest.main()
