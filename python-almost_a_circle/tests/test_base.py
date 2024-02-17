5776c8f07703bcaf6d842f45cf4a126164636964
Elvinooooo <Elvinooooo@github.com>
2024-02-17 13:51:11 -0800

    test case for pep validation added

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+"""Module checking unittest of Base.py"""
 import unittest
 import os
+import pep8
 from models.base import Base
 
 class TestBase(unittest.TestCase):
+    """Class for checking base.py"""
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
     def test_automatic_id(self):
+        """Check the automatic id assignment"""
         base1 = Base()
         base2 = Base()
         base3 = Base()
