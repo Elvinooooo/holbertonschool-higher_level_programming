f55c5c8bb1e706221da50427ac829cb53240dbdb
Elvinooooo <Elvinooooo@github.com>
2024-02-17 17:08:04 -0800

    changed

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -56,13 +56,3 @@ class Square(Rectangle):
             "x": self.x,
             "y": self.y
         }
-
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """Write the JSON string representation of list_objs to a file."""
-        if list_objs is None:
-            list_objs = []
-        filename = cls.__name__ + ".json"
-        with open(filename, mode='w') as file:
-            json_string = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
-            file.write(json_string)
